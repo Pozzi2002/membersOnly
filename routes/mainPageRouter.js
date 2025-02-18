ef9677040f07a00a72b064746abb5ccd6f2957af
Pozzi2002 <nik.stroganov.2019@mail.ru>
2025-02-18 20:33:59 +0300

    Create sign up page, but only html and css

--- routes/mainPageRouter.js
+++ routes/mainPageRouter.js
@@ -3,5 +3,6 @@ const mainPageRouter = express.Router();
 
 mainPageRouter.get('/', (req, res) => res.render('mainPage'));
 
+mainPageRouter.get('/sign-up', (req, res) => res.render("signUp"));
 
 module.exports = mainPageRouter;
